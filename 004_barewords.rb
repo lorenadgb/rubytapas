7bfb4a9cd412622635fe380b75e6ca93295ec804
Lorena Bento <lorenadgb@gmail.com>
2017-02-08 23:20:48 -0200

    BareWords: Base version

---
+++ 004_barewords.rb
@@ -0,0 +1,12 @@
+salutation = 'Most agreeable to see you'
+title = 'Commander'
+full_name = 'Sam Vimes'
+progname = 'Dis-organizer'
+version = 'Mark 7'
+designation = 'Seere'
+service_inquiry = 'order you a coffe'
+
+puts "#{salutation}, #{title}, #{full_name}. ",
+     "Welcome to #{progname} version #{version}. ",
+     "My name is #{designation}. ",
+     "May I #{service_inquiry}?"
